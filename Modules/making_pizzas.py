2703b4a32368539e40f33a76cc54cf5e882327b9
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2023-11-29 20:11:48 +0200

    Working with modules

---
+++ Modules/making_pizzas.py
@@ -0,0 +1,11 @@
+from pizza import make_pizza as mp
+# or
+# import pizza as p
+# p.make_pizza(16, 'pepperoni')
+
+# or
+# from pizza import *
+
+
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
