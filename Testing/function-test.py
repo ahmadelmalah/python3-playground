6b85d18121b1b3cf4b3e639aa92eaa69993803d4
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-04-04 02:41:40 +0200

    Adding a parametrizzed test

--- Testing/function-test.py
+++ Testing/function-test.py
@@ -1,7 +1,9 @@
+import pytest
 from function import sum
 
-def test_positive_numbers():
+def test_basic():
     assert sum(1,2) == 3
 
-def test_negative_numbers():
-    assert sum(-1,-2) == -3
+@pytest.mark.parametrize("a,b,expected", [(1,2,3), (2,3,5), (3,4,7)])
+def test_param(a,b,expected):
+    assert sum(a,b) == expected
