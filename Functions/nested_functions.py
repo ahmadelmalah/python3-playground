d46d8205d1a7905fff221a0a35f2175095d0a6e8
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-04-20 18:10:53 +0200

    nonlocal vars

--- Functions/nested_functions.py
+++ Functions/nested_functions.py
@@ -2,9 +2,11 @@ def level1():
     x = 1
 
     def level2():
-        y = 2
+        y = None
 
         def level3():
+            nonlocal y
+            y = 2
             z = 3
             print(x, y, z)
 
