e136366fc8ab7b46cff4e02901c4302e9a8515a3
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-04-20 18:27:00 +0200

    Math module

---
+++ handling_math.py
@@ -0,0 +1,60 @@
+import math
+
+x = math.sqrt(64)
+print(x)
+
+y = math.ceil(1.4)
+print(y)
+
+z = math.floor(1.4)
+print(z)
+
+# Constants
+print(math.pi)
+print(math.e)
+print(math.tau)
+print(math.inf)
+print(-math.inf)
+
+# Trigonometry
+print(math.sin(math.pi / 2))
+print(math.cos(math.pi))
+print(math.tan(math.pi / 4))
+
+# Degrees and Radians
+print(math.degrees(math.pi / 2))
+print(math.radians(90))
+
+# Hypotenuse
+print(math.hypot(3, 4))
+
+# Logarithms
+print(math.log(64, 2))
+print(math.log(64, 4))
+print(math.log10(1000))
+print(math.log2(32))
+
+# Power
+print(math.pow(2, 3))
+print(math.pow(3, 2))
+
+# Factorial
+print(math.factorial(5))
+
+# GCD
+print(math.gcd(60, 48))
+print(math.gcd(48, 60))
+
+# Degrees, Minutes, Seconds
+print(math.degrees(math.asin(1)))
+print(math.degrees(math.acos(1)))
+print(math.degrees(math.atan(1)))
+print(math.degrees(math.atan2(1, 1)))
+
+# Hyperbolic functions
+print(math.sinh(1))
+print(math.cosh(1))
+print(math.tanh(1))
+print(math.asinh(1))
+print(math.acosh(1))
+print(math.atanh(0.5))
