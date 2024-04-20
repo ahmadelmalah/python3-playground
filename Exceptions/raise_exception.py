a616f4c513485e50960ce24ef638fc3ee350034d
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-04-20 19:52:38 +0200

    raise exception

---
+++ Exceptions/raise_exception.py
@@ -0,0 +1,10 @@
+x = -1
+
+if x < 0:
+    raise Exception("Sorry, no numbers below zero")
+
+# OR with a specific error type
+# x = "hello"
+
+# if not type(x) is int:
+#     raise TypeError("Only integers are allowed")
