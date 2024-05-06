7f48398f2d5dfa92fc0b243f4689d76eacc82a43
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-05-07 02:55:32 +0300

    renaming files

---
+++ testing/test_function.py
@@ -0,0 +1,11 @@
+import pytest
+from sum import sum
+
+
+def test_basic():
+    assert sum(1, 2) == 3
+
+
+@pytest.mark.parametrize("a,b,expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7)])
+def test_param(a, b, expected):
+    assert sum(a, b) == expected
