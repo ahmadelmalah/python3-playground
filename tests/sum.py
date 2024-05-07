47bda7b92698b634fa3456fbe1743bacbce1de88
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-05-08 02:44:57 +0300

    reanming tests folder

---
+++ tests/sum.py
@@ -0,0 +1,6 @@
+def sum(x, y):
+    return x + y
+
+
+def myfunc_error():
+    raise ValueError("Exception 123 raised")
--- tests/test_states.py
+++ tests/test_states.py
@@ -1,5 +1,5 @@
 import pytest
-from sum import sum
+from sum import sum, myfunc_error
 
 
 # 2 failed
@@ -12,9 +12,18 @@ def test_fail2():
     assert sum(1, 2) == 3
 
 
-# 1 passed
+def test_dot_fail():
+    if sum(1, 2) != 4:
+        pytest.fail("This test failed because the sum is not 4")
+
+
+# 2 passed
 def test_pass():
     assert sum(1, 2) == 3
+
+
+def test_pass2():
+    assert None is None
 
 
 # 1 skipped
@@ -38,3 +47,8 @@ def test_sum_negative_raise():
 @pytest.mark.xfail()
 def test_sum_positive():
     assert sum(1, 2) == 3
+
+
+def test_match():
+    with pytest.raises(ValueError, match=r".* 123 .*"):
+        myfunc_error()
