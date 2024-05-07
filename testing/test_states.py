f129bd42ef9afdc0666bca4c10900186004a6294
Ahmad Elmalah <ahmedelmalah@hotmail.com>
2024-05-07 12:41:06 +0300

    Adding an example

--- testing/test_states.py
+++ testing/test_states.py
@@ -2,15 +2,28 @@ import pytest
 from sum import sum
 
 
-def test_pass():
-    assert sum(1, 2) == 3
-
-
+# 2 failed
 def test_fail():
     assert sum(1, 2) == 4
 
 
-# Xfailed
+def test_fail2():
+    x = 5 / 0
+    assert sum(1, 2) == 3
+
+
+# 1 passed
+def test_pass():
+    assert sum(1, 2) == 3
+
+
+# 1 skipped
+@pytest.mark.skip(reason="Do not run this test")
+def test_sum_skip():
+    assert sum(1, 2) == 3
+
+
+# 2 xfailed
 @pytest.mark.xfail()
 def test_sum_negative():
     assert sum(1, 2) == 4
@@ -21,13 +34,7 @@ def test_sum_negative_raise():
     raise Exception("This test is expected to fail")
 
 
-# XPassed
+# 1 xpassed
 @pytest.mark.xfail()
 def test_sum_positive():
-    assert sum(1, 2) == 3
-
-
-# Skipped
-@pytest.mark.skip(reason="Do not run this test")
-def test_sum_skip():
     assert sum(1, 2) == 3
